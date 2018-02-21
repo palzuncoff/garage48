21b9612927cc2ab67cdd0758589a7057f8f89421
Dmitrii Lazucov <palzuncoff@MacBook-Dmitrii.local>
2018-02-21 15:38:24 +0200

    Create Subject container

--- src/components/organisms/Subject/index.js
+++ src/components/organisms/Subject/index.js
@@ -1,17 +1,31 @@
 // https://github.com/diegohaz/arc/wiki/Atomic-Design
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
-const Subject = ({ name }) => {
-  return (
-    <div>
-      <h4>{name}</h4>
-      <Link to={`/subjects/${name}/topic1`}>Sub Topic - {name}</Link>
-      <Link to={`/subjects/${name}/topic2`}>Sub Topic - {name}</Link>
-      <Link to={`/subjects/${name}/topic3`}>Sub Topic - {name}</Link>
-    </div>
+class Subject extends Component {
+  state = {
+    topics: [],
+  }
 
-  )
+  componentWillMount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>{this.props.name}</h4>
+        <Link to={`/subjects/${name}/topic1`}>Sub Topic - {name}</Link>
+        <Link to={`/subjects/${name}/topic2`}>Sub Topic - {name}</Link>
+        <Link to={`/subjects/${name}/topic3`}>Sub Topic - {name}</Link>
+      </div>
+    );
+  }
+};
+
+Subject.propTypes = {
+  name: PropTypes.string.isRequired,
 }
 
 export default Subject
---
+++ src/containers/Subject.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+import { Subject } from 'components'
+
+export default connect()(Subject)
